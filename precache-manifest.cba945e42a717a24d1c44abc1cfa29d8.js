9a015dbf2f7ab9134eeeb03d50f4533cb1c6d1d0
bbradforddesign <bbradforddesign@gmail.com>
2020-04-24 21:39:45 -0500

    Updates

---
+++ precache-manifest.cba945e42a717a24d1c44abc1cfa29d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccf0e541b36f9c585e7b71cc73a5331",
+    "url": "/doe/index.html"
+  },
+  {
+    "revision": "00f9bea329bbbfec507a",
+    "url": "/doe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2045a4a0aa0b382cb286",
+    "url": "/doe/static/js/2.cd06cfbc.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/doe/static/js/2.cd06cfbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f9bea329bbbfec507a",
+    "url": "/doe/static/js/main.9b11753e.chunk.js"
+  },
+  {
+    "revision": "93b81bf75131ecca40b0",
+    "url": "/doe/static/js/runtime-main.f77332d5.js"
+  }
+]);
